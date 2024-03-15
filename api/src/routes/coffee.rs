e18995bc29861f4ffc66c8f4d05fef7e5b80b238
langered <denislanger@hotmail.de>
2024-03-15 22:01:12 +0100

    feat: Add post endpoint

--- api/src/routes/coffee.rs
+++ api/src/routes/coffee.rs
@@ -6,26 +6,54 @@ use rocket::{
 
 #[derive(Deserialize, Serialize)]
 struct Coffee {
+    brand: String,
     name: String,
+    grind_size: u8,
+    grind_time: f32,
+    rating: Option<u8>,
 }
 
 #[rocket::get("/coffees")]
 fn coffee() -> Json<Vec<Coffee>> {
     let coffees = vec![
         Coffee {
-            name: "Americano".to_string(),
+            brand: "Agata".to_string(),
+            name: "House Blend Espresso".to_string(),
+            grind_size: 8,
+            grind_time: 6.5,
+            rating: Some(4),
         },
         Coffee {
-            name: "Latte".to_string(),
+            brand: "Agata".to_string(),
+            name: "Fernando Escobar".to_string(),
+            grind_size: 8,
+            grind_time: 7.5,
+            rating: Some(3),
         },
         Coffee {
-            name: "Cappuccino".to_string(),
+            brand: "Agata".to_string(),
+            name: "Henrique & Ricardo Barbosa".to_string(),
+            grind_size: 8,
+            grind_time: 6.5,
+            rating: Some(5),
+        },
+        Coffee {
+            brand: "Agata".to_string(),
+            name: "Finca Rio Colorado".to_string(),
+            grind_size: 7,
+            grind_time: 6.5,
+            rating: None,
         },
     ];
 
     Json(coffees)
 }
 
+#[post("/coffee", format = "json", data = "<user_input>")]
+fn create_coffee(user_input: Json<Coffee>) -> Json<Coffee> {
+    user_input
+}
+
 pub fn routes() -> Vec<Route> {
-    routes![coffee]
+    routes![coffee, create_coffee]
 }
